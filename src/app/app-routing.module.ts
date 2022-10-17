ba476d3a0299b9f4dc4740efbe518230be7d5176
Chakri <chakradhar@addant.com>
2022-10-17 11:38:08 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/Components/login/login.component';
+import { LayoutComponent } from './shared/Components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
-  }
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent
+  },
 ];
 
 @NgModule({
