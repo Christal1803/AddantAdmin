9e35a5e5369f8adc413769f071257f746b7c6080
Chakri <chakradhar@addant.com>
2022-11-24 09:13:04 +0530

    hellokitty

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { BlogModule } from '../pages/blog/blog.module';
+import { TestComponent } from './Components/test/test.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    TestComponent
   ],
   imports: [
     CommonModule,
